59dde38de70f5c24897facfa84f16b54bf97b206
unknown <jvfernandes.contato@gmail.com>
2022-05-10 11:40:10 -0300

    Configurando o Eslint e o editorconfig

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (request, response) =>{
-    response.send('Hello World!')
-})
+app.get('/', (request, response) => {
+  response.send('Hello World!');
+});
 
-app.listen(3000,() => console.log('🔥 Server started at http://localhost:3000'))
+app.listen(3000, () => console.log('🔥 Server started at http://localhost:3000'));
