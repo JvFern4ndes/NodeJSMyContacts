c73c584182c52bec2b6d5ab3f8e969ec000e2909
unknown <jvfernandes.contato@gmail.com>
2022-05-17 14:16:18 -0300

    Criando o Header da aplicação

---
+++ fe/src/components/App/index.js
@@ -0,0 +1,22 @@
+import { ThemeProvider } from 'styled-components';
+
+import GlobalStyles from '../../assets/styles/global';
+import defaultTheme from '../../assets/styles/themes/default';
+
+import Header from '../Header';
+
+import { Container } from './styles';
+
+function App() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyles />
+
+      <Container>
+        <Header />
+      </Container>
+    </ThemeProvider>
+  );
+}
+
+export default App;
---
+++ fe/src/components/App/styles.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 100%auto;
+  max-width: 500px;
+  margin: 0 auto;
+`;
