9f4ef294d03fb721261fc4d7d3cfb98699b1edb6
unknown <jvfernandes.contato@gmail.com>
2022-05-18 19:53:56 -0300

    Criando o componente Button

--- fe/src/pages/NewContact/index.js
+++ fe/src/pages/NewContact/index.js
@@ -2,6 +2,7 @@ import PageHeader from '../../components/PageHeader';
 
 import Input from '../../components/Input';
 import Select from '../../components/Select';
+import Button from '../../components/Button';
 
 export default function NewContact() {
   return (
@@ -18,6 +19,12 @@ export default function NewContact() {
         <option value="123">Instagram</option>
         <option value="123">Instagram</option>
       </Select>
+      <Button type="button">
+        Salvar alterações
+      </Button>
+      <Button type="button" disabled>
+        Salvar alterações
+      </Button>
     </>
   );
 }
