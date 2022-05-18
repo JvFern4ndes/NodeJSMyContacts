6f7eb933c93f30c670553f4eb5d724c15c2c7b1f
unknown <jvfernandes.contato@gmail.com>
2022-05-18 19:19:44 -0300

    Criando os componentes Input e Select

--- fe/src/pages/NewContact/index.js
+++ fe/src/pages/NewContact/index.js
@@ -1,9 +1,23 @@
 import PageHeader from '../../components/PageHeader';
 
+import Input from '../../components/Input';
+import Select from '../../components/Select';
+
 export default function NewContact() {
   return (
-    <PageHeader
-      title="Novo contato"
-    />
+    <>
+      <PageHeader
+        title="Novo contato"
+      />
+
+      <Input type="text" placeholder="Nome" />
+      <Select>
+        <option value="123">Instagram</option>
+        <option value="123">Instagram</option>
+        <option value="123">Instagram</option>
+        <option value="123">Instagram</option>
+        <option value="123">Instagram</option>
+      </Select>
+    </>
   );
 }
