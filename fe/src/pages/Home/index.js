0ca0aa064590ccb7fef7f36d6ed22c441a6801fd
unknown <jvfernandes.contato@gmail.com>
2022-05-20 12:27:38 -0300

    Criando o componente Loader

--- fe/src/pages/Home/index.js
+++ fe/src/pages/Home/index.js
@@ -7,12 +7,12 @@ import {
 import arrow from '../../assets/images/icons/arrow.svg';
 import edit from '../../assets/images/icons/edit.svg';
 import trash from '../../assets/images/icons/trash.svg';
-import Modal from '../../components/Modal';
+import Loader from '../../components/Loader';
 
 export default function Home() {
   return (
     <Container>
-      <Modal danger />
+      <Loader />
 
       <InputSearchContainer>
         <input type="text" placeholder="Pesquise pelo nome..." />
