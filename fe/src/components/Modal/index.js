6808aaebf16d91e9966c3bf25d1f27bcb5aa06b3
unknown <jvfernandes.contato@gmail.com>
2022-05-20 10:26:57 -0300

    Criando o componente Modal

--- fe/src/components/Modal/index.js
+++ fe/src/components/Modal/index.js
@@ -1,9 +1,25 @@
-import { Overlay } from './styles';
+import { Overlay, Container, Footer } from './styles';
+
+import Button from '../Button';
 
 export default function Modal() {
   return (
     <Overlay>
-      Modal
+      <Container>
+        <h1>Titulo do modal</h1>
+        <p>
+          Corpo do modal
+        </p>
+
+        <Footer>
+          <button type="button" className="cancel-button">
+            Cancelar
+          </button>
+          <Button type="button">
+            Deletar
+          </Button>
+        </Footer>
+      </Container>
     </Overlay>
   );
 }
