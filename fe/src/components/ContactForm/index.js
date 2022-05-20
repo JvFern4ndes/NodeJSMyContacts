62e0cf526627fbdff8675e0f1727f68fed944ad4
unknown <jvfernandes.contato@gmail.com>
2022-05-20 12:53:37 -0300

    Atribuindo eventos a campos de formulários

--- fe/src/components/ContactForm/index.js
+++ fe/src/components/ContactForm/index.js
@@ -11,7 +11,10 @@ export default function ContactForm({ buttonlabel }) {
   return (
     <Form>
       <FormGroup>
-        <Input placeholder="Nome" />
+        <Input
+          placeholder="Nome"
+          onChange={(event) => console.log(event.target.value)}
+        />
       </FormGroup>
 
       <FormGroup
