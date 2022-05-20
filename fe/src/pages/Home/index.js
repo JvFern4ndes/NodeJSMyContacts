5f8e4b6df20572a8b8757de0005aac38f4906c9a
unknown <jvfernandes.contato@gmail.com>
2022-05-20 12:39:26 -0300

    Status de erro no FormGroup e no Input

--- fe/src/pages/Home/index.js
+++ fe/src/pages/Home/index.js
@@ -7,12 +7,10 @@ import {
 import arrow from '../../assets/images/icons/arrow.svg';
 import edit from '../../assets/images/icons/edit.svg';
 import trash from '../../assets/images/icons/trash.svg';
-import Loader from '../../components/Loader';
 
 export default function Home() {
   return (
     <Container>
-      <Loader />
 
       <InputSearchContainer>
         <input type="text" placeholder="Pesquise pelo nome..." />
