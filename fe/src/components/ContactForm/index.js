99dc370c6f97bf70f3a1447d289dc660ecbd0e5e
unknown <jvfernandes.contato@gmail.com>
2022-05-22 14:31:29 -0300

    Pegando os dados e lidando com o submit do form

--- fe/src/components/ContactForm/index.js
+++ fe/src/components/ContactForm/index.js
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
 
-import { useState, useRef } from 'react';
+import { useState } from 'react';
 import { Form, ButtonContainer } from './styles';
 
 import FormGroup from '../FormGroup';
@@ -10,43 +10,52 @@ import Button from '../Button';
 
 export default function ContactForm({ buttonlabel }) {
   const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+  const [category, setCategory] = useState('');
 
-  const emailInput = useRef(null);
+  function handleSubmit(event) {
+    event.preventDefault();
 
-  function handleClick() {
-    console.log(emailInput.current.value);
+    console.log({
+      name, email, phone, category,
+    });
   }
 
-  console.log('renderizou');
-
   return (
-    <Form>
-      <button type="button" onClick={handleClick}>
-        Loga emailInput
-      </button>
+    <Form onSubmit={handleSubmit}>
       <FormGroup>
         <Input
-          value={name}
           placeholder="Nome"
+          value={name}
           onChange={(event) => setName(event.target.value)}
         />
       </FormGroup>
 
       <FormGroup>
         <Input
-          defaultValue="JoãoVictor@email.com"
           placeholder="E-mail"
-          ref={emailInput}
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
         />
       </FormGroup>
 
       <FormGroup>
-        <Input placeholder="Telefone" />
+        <Input
+          placeholder="Telefone"
+          value={phone}
+          onChange={(event) => setPhone(event.target.value)}
+        />
       </FormGroup>
 
       <FormGroup>
-        <Select>
+        <Select
+          value={category}
+          onChange={(event) => setCategory(event.target.value)}
+        >
+          <option value="">Categoria</option>
           <option value="instagram">Instagram</option>
+          <option value="discord">Discord</option>
         </Select>
       </FormGroup>
 
