6058f94a07dcb96b4161d05a6df436a507c095d3
unknown <jvfernandes.contato@gmail.com>
2022-05-28 20:35:59 -0300

    Rodando o back e o front em paralelo

--- API/src/index.js
+++ API/src/index.js
@@ -13,4 +13,4 @@ app.use((error, request, response, next) => {
   response.sendStatus(500);
 });
 
-app.listen(3000, () => console.log('🔥 Server started at http://localhost:3000'));
+app.listen(3001, () => console.log('🔥 Server started at http://localhost:3000'));
