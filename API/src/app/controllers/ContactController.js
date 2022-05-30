1fab4517312cd81635d2f221aab2c8bc9060febf
unknown <jvfernandes.contato@gmail.com>
2022-05-30 18:54:06 -0300

    Criando o middleware do CORS na API

--- API/src/app/controllers/ContactController.js
+++ API/src/app/controllers/ContactController.js
@@ -5,7 +5,6 @@ class ContactController {
     const { orderBy } = request.query;
     const contacts = await ContactsRepository.findAll(orderBy);
 
-    response.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
     response.json(contacts);
   }
 
