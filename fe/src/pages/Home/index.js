0215cde745629aec4155cf13566a44e05fd81cc3
unknown <jvfernandes.contato@gmail.com>
2022-05-31 18:34:26 -0300

    Fazendo o parse do body da response

--- fe/src/pages/Home/index.js
+++ fe/src/pages/Home/index.js
@@ -53,14 +53,13 @@ export default function Home() {
   );
 }
 
-fetch('http://localhost:3001/contacts', {
-  method: 'DELETE',
-  headers: new Headers({
-    'X-App-ID': '123',
-  }),
-})
-  .then((response) => {
+fetch('http://localhost:3001/contacts')
+  .then(async (response) => {
+    const json = await response.json();
     console.log('response', response);
+    json.forEach((contact) => {
+      console.log(contact.name);
+    });
   })
   .catch((error) => {
     console.log('erro', error);
