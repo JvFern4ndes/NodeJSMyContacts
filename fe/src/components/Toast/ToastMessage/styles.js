d0c7c4b1ce26e389e1faf7521e2e8ce66fef1569
unknown <jvfernandes.contato@gmail.com>
2022-07-20 13:45:45 -0300

    Trabalhando com variantes no Styled-Components

--- fe/src/components/Toast/ToastMessage/styles.js
+++ fe/src/components/Toast/ToastMessage/styles.js
@@ -1,14 +1,27 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+
+const containerVariants = {
+  default: css`
+  background: ${({ theme }) => theme.colors.primary.main};
+  `,
+  success: css`
+    background: ${({ theme }) => theme.colors.success.main};
+  `,
+  danger: css`
+    background: ${({ theme }) => theme.colors.danger.main};
+  `,
+};
 
 export const Container = styled.div`
   padding: 16px 32px;
-  background: ${({ theme }) => theme.colors.primary.main};
   color: #fff;
   border-radius: 4px;
   box-shadow: 0px 20px 20px -16px rgba(0, 0, 0, 0.25);
   display: flex;
   align-items: center;
   justify-content: center;
+
+  ${({ type }) => containerVariants[type] || containerVariants.default}
 
   & + & {
     margin-top: 12px;
