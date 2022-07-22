4c2ac828e728547ab89d5a5485ce6234d77c4887
unknown <jvfernandes.contato@gmail.com>
2022-07-22 18:14:01 -0300

    Criando eventos customizados com a API de CustomEvents do JavaScript

--- fe/src/components/Toast/ToastContainer/index.js
+++ fe/src/components/Toast/ToastContainer/index.js
@@ -1,14 +1,27 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Container } from './styles';
 
 import ToastMessage from '../ToastMessage';
 
 export default function ToastContainer() {
-  const [messages] = useState([
-    { id: Math.random(), type: 'default', text: 'Default text' },
-    { id: Math.random(), type: 'danger', text: 'danger text' },
-    { id: Math.random(), type: 'success', text: 'Success text' },
-  ]);
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    function handleAddToast(event) {
+      const { type, text } = event.detail;
+
+      setMessages((prevState) => [
+        ...prevState,
+        { id: Math.random(), type, text },
+      ]);
+    }
+
+    document.addEventListener('addtoast', handleAddToast);
+
+    return () => {
+      document.removeEventListener('addtoast', handleAddToast);
+    };
+  }, []);
 
   return (
     <Container>
