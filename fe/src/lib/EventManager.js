00701b25a4cba69cbda95dd6f2b2132dbed57483
unknown <jvfernandes.contato@gmail.com>
2022-07-25 16:26:50 -0300

    Criando métodos para adicionar listeners e emitir eventos pt2

--- fe/src/lib/EventManager.js
+++ fe/src/lib/EventManager.js
@@ -2,7 +2,18 @@ export default class EventManager {
   constructor() {
     this.listeners = {};
   }
+
+  on(event, listener) {
+    if (!this.listeners[event]) {
+      this.listeners[event] = [];
+    }
+
+    this.listeners[event].push(listener);
+  }
 }
 
 const toastEventManager = new EventManager();
+
+toastEventManager.on('addtoast', () => {});
+
 console.log(toastEventManager);
