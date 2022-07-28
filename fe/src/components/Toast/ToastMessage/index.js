6a78410b49d6891fbe071c4e23cc41f305cff265
unknown <jvfernandes.contato@gmail.com>
2022-07-28 18:59:12 -0300

    Removendo os Toasts automaticamente após um período de tempo

--- fe/src/components/Toast/ToastMessage/index.js
+++ fe/src/components/Toast/ToastMessage/index.js
@@ -1,11 +1,22 @@
 import PropTypes from 'prop-types';
 
+import { useEffect } from 'react';
 import { Container } from './styles';
 
 import xCircleIcon from '../../../assets/images/icons/x-circle.svg';
 import checkCircleIcon from '../../../assets/images/icons/check-circle.svg';
 
 export default function ToastMessage({ message, onRemoveMessage }) {
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      onRemoveMessage(message.id);
+    }, message.duration || 7000);
+
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, [message, onRemoveMessage]);
+
   function handleRemoveToast() {
     onRemoveMessage(message.id);
   }
@@ -29,6 +40,7 @@ ToastMessage.propTypes = {
     id: PropTypes.number.isRequired,
     text: PropTypes.string.isRequired,
     type: PropTypes.oneOf(['default', 'success', 'danger']),
+    duration: PropTypes.number,
   }).isRequired,
 
   onRemoveMessage: PropTypes.func.isRequired,
