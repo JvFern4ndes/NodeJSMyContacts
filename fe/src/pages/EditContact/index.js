46ae34665c1697b6c7b75d2222fdb395e592fafe
unknown <jvfernandes.contato@gmail.com>
2022-07-30 18:21:26 -0300

    Carregando os dados do contato na página de edição

--- fe/src/pages/EditContact/index.js
+++ fe/src/pages/EditContact/index.js
@@ -1,15 +1,55 @@
+import { useParams, useHistory } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+
 import PageHeader from '../../components/PageHeader';
 import ContactForm from '../../components/ContactForm';
+import Loader from '../../components/Loader';
+
+import ContactsService from '../../services/ContactsService';
+import toast from '../../utils/toast';
 
 export default function EditContact() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  const { id } = useParams();
+  const history = useHistory();
+
+  useEffect(() => {
+    async function loadContact() {
+      try {
+        const contactData = await ContactsService.getContactById(
+          id,
+        );
+
+        console.log({ contactData });
+        setIsLoading(false);
+      } catch {
+        history.push('/');
+        toast({
+          type: 'danger',
+          text: 'Contato não encontrado!',
+        });
+      }
+    }
+
+    loadContact();
+  }, [id, history]);
+
+  function handleSubmit() {
+    //
+  }
+
   return (
     <>
+      <Loader isLoading={isLoading} />
+
       <PageHeader
         title="Editar Mateus Silva"
       />
 
       <ContactForm
         buttonlabel="Salvar alterações"
+        onSubmit={handleSubmit}
       />
     </>
   );
